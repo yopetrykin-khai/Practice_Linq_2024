2d2ae675dcf88e18569930c4b05fe4ac12bad8d0
Yevheniy Petrykin <y.o.petrykin@student.khai.edu>
2024-12-25 14:01:27 +0200

    Query 3 commit

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -84,10 +84,14 @@ namespace Practice_Linq_2024
         {
             //Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games.Where(t => t.Country == "France" && t.Home_team == "France" && t.Date.Year == 2021 && t.Away_score == t.Home_score); ;   // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 3 ========================");
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date.ToShortDateString()} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
             // див. приклад як має бути виведено:
 
